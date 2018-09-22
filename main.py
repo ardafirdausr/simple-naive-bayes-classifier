518a3c69518ac6cff842664d349da610727b73eb
Kevin Haidar <kevin.haidar24@gmail.com>
2018-09-22 13:35:54 +0700

    Update main.py
    function mean and standart deviation

--- main.py
+++ main.py
@@ -11,6 +11,16 @@ for row in table:
     data.append(row)
 print(data)
 
+#Hitung mean
+def mean(numbers):
+	return sum(numbers)/float(len(numbers))
+
+#hitung Standar Deviasi
+def stdev(numbers):
+	avg = mean(numbers)
+	variance = sum([pow(x-avg,2) for x in numbers])/float(len(numbers)-1)
+	return math.sqrt(variance)
+
 
 # close file
 file.close()
